c5b189a21c830611f843495f60b87fd9e8d95709
Lucas Neves Pires <npires.lucas@gmail.com>
2020-11-22 13:21:43 -0300

    feign client

--- hr-payroll/src/main/java/br/com/lucasnpires/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/lucasnpires/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package br.com.lucasnpires.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
--- hr-payroll/src/main/java/br/com/lucasnpires/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/lucasnpires/hrpayroll/services/PaymentService.java
@@ -1,33 +1,21 @@
 package br.com.lucasnpires.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.lucasnpires.hrpayroll.entities.Payment;
 import br.com.lucasnpires.hrpayroll.entities.Worker;
+import br.com.lucasnpires.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-		
+
 	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
+	private WorkerFeignClient workerFeignClient;
+
 	public Payment getPayment(Long workerId, Integer days) {
-		
-		Map<String, String> uriVariables = new HashMap<String, String>();
-		uriVariables.put("id", workerId.toString());
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-		
-		
-		return Payment.builder().name(worker.getName()).dailyIncome(worker.getDailyIncome()).days(days).build();				
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+
+		return Payment.builder().name(worker.getName()).dailyIncome(worker.getDailyIncome()).days(days).build();
 	}
 }
