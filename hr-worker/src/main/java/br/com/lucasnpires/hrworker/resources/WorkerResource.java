a5cfae1b941604a3555d9d6990fb9f8fed9536a6
Lucas Neves Pires <npires.lucas@gmail.com>
2020-11-22 13:33:28 -0300

    add ribbon load balancing

--- hr-worker/src/main/java/br/com/lucasnpires/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/lucasnpires/hrworker/resources/WorkerResource.java
@@ -3,6 +3,7 @@ package br.com.lucasnpires.hrworker.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucasnpires.hrworker.entities.Worker;
 import br.com.lucasnpires.hrworker.repositories.WorkerRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping(value = "/workers")
+@Slf4j
 public class WorkerResource {
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private WorkerRepository repository;
@@ -27,6 +33,8 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	private ResponseEntity<Worker> findById(@PathVariable Long id){
+		log.info("PORT = " + env.getProperty("local.server.port"));
+		
 		return ResponseEntity.ok(repository.findById(id).orElse(new Worker()));
 	}
 	
