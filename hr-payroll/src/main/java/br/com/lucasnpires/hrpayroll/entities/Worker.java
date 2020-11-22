f35254748e7a098c4463bed924750b72c5509bfe
Lucas Neves Pires <npires.lucas@gmail.com>
2020-11-22 13:09:54 -0300

    rest template

--- hr-payroll/src/main/java/br/com/lucasnpires/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/br/com/lucasnpires/hrpayroll/entities/Worker.java
@@ -1,12 +1,6 @@
-package br.com.lucasnpires.hrworker.entities;
+package br.com.lucasnpires.hrpayroll.entities;
 
 import java.io.Serializable;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,18 +8,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "tb_worker")
 public class Worker implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
--- hr-payroll/src/main/java/br/com/lucasnpires/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/lucasnpires/hrpayroll/services/PaymentService.java
@@ -1,14 +1,33 @@
 package br.com.lucasnpires.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.lucasnpires.hrpayroll.entities.Payment;
+import br.com.lucasnpires.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+		
+	@Autowired
+	private RestTemplate restTemplate;
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
 	
 	public Payment getPayment(Long workerId, Integer days) {
-		return Payment.builder().name("Bob").dailyIncome(200.00).days(days).build();				
+		
+		Map<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("id", workerId.toString());
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		
+		
+		return Payment.builder().name(worker.getName()).dailyIncome(worker.getDailyIncome()).days(days).build();				
 	}
 }
