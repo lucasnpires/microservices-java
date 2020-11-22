cd7beb46d77b2eca2893bc7d8a7147f5613606ba
Lucas Neves Pires <npires.lucas@gmail.com>
2020-11-22 13:48:49 -0300

    add eureka server

---
+++ hr-eureka-server/src/test/java/br/com/lucasnpires/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasnpires.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
